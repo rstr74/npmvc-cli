c0659025966d084c9aff2b0fb92c304394cbcb67
rstr74 <robbertstreng2012@gmail.com>
2015-07-30 15:45:57 +0200

    Commands

---
+++ template/Commands.js
@@ -0,0 +1,22 @@
+/**
+* {FILE} 
+* {GENERATED}
+* 
+* @author {AUTHOR}
+* @email {EMAIL}
+* 
+*/
+
+module.exports = function(include, puremvc) {
+	puremvc.define(
+		// CLASS INFO
+		{
+			name: '{NAME_SPACE}.{CLASS_NAME}'
+		},
+		// INSTANCE MEMBERS
+		{},
+		// CONSTANTS
+		{
+			SOME_COMMAND:"SOME_COMMAND"
+		});
+}
